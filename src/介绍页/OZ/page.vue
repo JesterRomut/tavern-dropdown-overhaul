f31e17c56c9f364f4b9b5f9131d37bd40eea342f
JesterRomut <lxcmoriah@gmail.com>
2026-08-07 13:57:55 +0800

    1

--- src/介绍页/OZ/page.vue
+++ src/介绍页/OZ/page.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <p>作者@Kernschmelze。</p>
+    <p>作者@Kernschmelze。OZ，欧几里得。</p>
     <p>“大家早就不需要超能力者了。有了无人机和计算机，谁还需要被折弯的勺子和被撬开的锁呢？”</p>
     <section>
       <h3>开场一览</h3>
@@ -8,6 +8,7 @@
         <li v-for="msg in starts" :key="msg">{{ msg }}</li>
       </ol>
     </section>
+    <p>新版酒馆点击右下角箭头下的1/{{ starts.length }}，可快速跳转开场。</p>
     <p>
       非自用的二改（任何修改）都需授权。二传需标明作者及原帖
       <br />
@@ -22,9 +23,13 @@
 <script setup lang="ts">
 const starts = [
   '（自定义开局/此介绍页不会发送给AI）',
+  'user和OZ争抢半价便当',
   'OZ来user家面试挖掘失传媒体的工作',
-  'user在图书馆发现了看书中的OZ',
-  'OZ掀开衣服露出小腹上的纹O身，本以为是魅魔淫纹，没想到是……',
+  'user在图书馆发现了看书的OZ',
+  'user在实验室旧址拍到了隐身的OZ',
+  'user跟OZ在野外徒步',
+  'user看见OZ被雨打湿衣服透出的纹O身',
+  '爱爱时OZ掀开衣服露出小腹上的纹O身，本以为是魅魔淫纹，没想到是……',
 ];
 </script>
 <style>
@@ -110,116 +115,5 @@ main > footer h2 {
   font-weight: lighter;
   font-size: 0.9em;
   text-transform: uppercase;
-}
-
-@media screen and (max-width: 600px) {
-  .segmented {
-    padding: 0.5em 0;
-    display: flex;
-    font-size: 0.85em;
-    /* background: linear-gradient(transparent 45%, #1a1c20, transparent 55%); */
-    justify-content: center;
-  }
-
-  .segmented-button {
-    transition: 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    border-radius: 6px;
-    flex-grow: 1;
-    text-align: center;
-    background-color: transparent;
-    input {
-      position: absolute;
-      width: 1px;
-      height: 1px;
-      padding: 0;
-      margin: -1px;
-      overflow: hidden;
-      clip: rect(0, 0, 0, 0);
-      white-space: nowrap;
-      border-width: 0;
-    }
-
-    &:hover {
-      background-color: #333537;
-    }
-
-    &:has(:checked) {
-      background-color: #1a1c20;
-    }
-
-    &:has(:focus-visible) {
-      outline: 2px solid #007acc;
-    }
-  }
-}
-/** https://freefrontend.com/css-radio-buttons/ */
-@media screen and (min-width: 600px) {
-  .segmented {
-    --padding: 0.2rem;
-    display: flex;
-    padding: var(--padding);
-    border-radius: 9999px;
-    anchor-name: --segmented-button-hover;
-    font-size: 0.85em;
-
-    &::before,
-    &::after {
-      content: '';
-      position: absolute;
-      border-radius: inherit;
-      transition: inset 250ms cubic-bezier(0.4, 0, 0.2, 1);
-      inset: calc(anchor(start) + var(--padding)) calc(anchor(end) + var(--padding)) calc(anchor(end) + var(--padding))
-        calc(anchor(start) + var(--padding));
-    }
-
-    &::before {
-      background-color: #1a1c20;
-      position-anchor: --segmented-button-hover;
-    }
-
-    &::after {
-      background-color: #333537;
-      position-anchor: --segmented-button-checked;
-      box-shadow:
-        0 0 0 1px #484a4c,
-        0 1px 3px 0 rgb(0 0 0 / 0.1),
-        0 1px 2px -1px rgb(0 0 0 / 0.1);
-    }
-  }
-
-  .segmented-button {
-    background-color: transparent;
-    border: none;
-    border-radius: inherit;
-    padding: 0.65rem 1.3rem;
-    font-size: inherit;
-    cursor: default;
-    z-index: 1;
-    anchor-name: var(--anchor-name-1, --a), var(--anchor-name-2, --b);
-
-    &:hover {
-      --anchor-name-1: --segmented-button-hover;
-    }
-
-    &:has(:checked) {
-      --anchor-name-2: --segmented-button-checked;
-    }
-
-    &:has(:focus-visible) {
-      outline: 2px solid #007acc;
-    }
-
-    input {
-      position: absolute;
-      width: 1px;
-      height: 1px;
-      padding: 0;
-      margin: -1px;
-      overflow: hidden;
-      clip: rect(0, 0, 0, 0);
-      white-space: nowrap;
-      border-width: 0;
-    }
-  }
 }
 </style>
